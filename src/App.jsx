e01a1224e6bf5259daec9ec52e6c22488d88563d
Kenny Ho <kennyho.code@gmail.com>
2023-10-24 13:22:10 -0400

    needs work with tailwind and ssr:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, lazy, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
@@ -25,29 +24,26 @@ const App = () => {
         background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Suspense fallback={<h1>Loading route...</h1>}>
-            <AdoptedPetContext.Provider value={adoptedPet}>
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  <h1>Adopt Me!</h1>
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </AdoptedPetContext.Provider>
-          </Suspense>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<h1>Loading route...</h1>}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </Suspense>
+      </QueryClientProvider>
     </div>
   );
 };
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+
+export default App;
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
